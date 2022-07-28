9f1c07d291a710e20281f6133f72a3b23bc35999
ronaldidro <rjdiazi10@gmail.com>
2022-07-28 11:22:37 -0500

    default redirect route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import { Container } from '@chakra-ui/react'
 import CompanyProducts from './components/CompanyProducts'
 import Footer from './components/Footer'
@@ -12,6 +12,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/compania/:id" element={<CompanyProducts />} />
         <Route path="/pedido" element={<OrderDetail />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
       <Footer />
     </Container>
